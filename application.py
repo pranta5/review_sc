5babf56d2aeb60d8b3db684870143fbe725dfd99
Pranta Das <122752537+pranta5@users.noreply.github.com>
2023-03-02 15:17:05 +0530

    Update application.py

--- application.py
+++ application.py
@@ -71,9 +71,9 @@ def index():
                 mydict = {"Product": searchString, "Name": name, "Rating": rating, "CommentHead": commentHead,
                           "Comment": custComment}
                 reviews.append(mydict)
-            client = pymongo.MongoClient("mongodb+srv://pwskills:pwskills@cluster0.ln0bt5m.mongodb.net/?retryWrites=true&w=majority")
-            db = client['review_scrap']
-            review_col = db['review_scrap_data']
+           client = pymongo.MongoClient("mongodb+srv://prantadas:prantadas@cluster0.dmwe3ec.mongodb.net/?retryWrites=true&w=majority")
+            db = client["review_scrap"]
+            review_col=db["review_scrap_data"]
             review_col.insert_many(reviews)
             return render_template('results.html', reviews=reviews[0:(len(reviews)-1)])
         except Exception as e:
